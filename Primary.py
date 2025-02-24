06179957ac127cc95936ffb39d17a0d80ec6efac
Simoneb24 <brooks.simone@pingree.org>
2025-02-24 11:59:47 -0500

    commited

--- Primary.py
+++ Primary.py
@@ -3,24 +3,48 @@ from flet.core import page
 #import data_gather
 import flet as ft
 from flet.core.types import TextAlign
-from Homework1 import TODONOW
-from data_gather import DataGather
+#from Homework import HomeworkApp
 
+from data_gather import DataGather
 datagath = DataGather()
 DataGather()
+grabpass = open("infoPassing.txt", 'r')
+name = grabpass.readline()
+print("hii " + name)
+for line in grabpass:
+    line.strip()
+grabpass.close()
 print("blub")
 def mains(page: ft.Page):
+    place = "Homework"
     page.bgcolor = ft.Colors.INDIGO_300
-    name = ft.TextField(hint_text="can you repeat your name enter here and press plus when your done\n i know we literaly just asked this but for now please retype as same syntax as before")
+    #name = ft.TextField(hint_text="can you repeat your name enter here and press plus when your done\n i know we literaly just asked this but for now please retype as same syntax as before")
     def Todo(e):
-        #Toodo = TODONOW()
-        #Toodo()
-        print("to-do")
+        #todotime()
+        grabpass = open("infoPassing.txt", 'w')
+        grabpass.write("\n")
+        grabpass.write("todo")
+        grabpass.close()
+        page.window.close()
+        return
+        #Homeworkcl(ft.Row)
+        #HomeworkApp()
     # signifies the user wants to view the calender
+    def ExtraCuriculars(e):
+        print("extracuriculars called")
+        grabpass = open("infoPassing.txt", 'w')
+        grabpass.write("\n")
+        grabpass.write("extracuriculars")
+        grabpass.close()
+        page.window.close()
     def calender(e):
-        print("calender")
+        grabpass = open("infoPassing.txt", 'w')
+        grabpass.write("\n")
+        grabpass.write("calender")
+        grabpass.close()
+        page.window.close()
     #signifies the script is ready to move on
-    def haveName(e):
+    def haveName():
         page.clean()
         page.bgcolor = ft.Colors.INDIGO_500
         page.add(ft.TextField(hint_text="Welcome, click the drop down menu to see your to-do lists or calender", text_size=20, color=ft.Colors.WHITE))
@@ -32,15 +56,29 @@ def mains(page: ft.Page):
                 ),
                 ft.PopupMenuItem(
                     text="To-Do lists", checked=False, on_click=Todo
+                ),
+                ft.PopupMenuItem(
+                    text="Manage Extracurriculars", checked=False, on_click=ExtraCuriculars
                 )
             ]
         )
         page.add(pb)
     #enter button calling haveName function
-    page.add(name, ft.FloatingActionButton(icon=ft.Icons.ADD, on_click=haveName))
+    haveName()
+    #page.add(name, ft.FloatingActionButton(icon=ft.Icons.ADD, on_click=haveName))
 
 #allows script to run independantly
 ft.app(mains)
+from Homework import Toodo
+
+#from Extracurricular import Extracurricular
+#createcalender()
+
+
+#TODONOW = Toodo()
+#Toodo()
+
+
 
 
 
